7366b4b9751553e8fc87f54a71afe4bf93d11e27
Bryan Cole <bryan.cole@semiotic.ai>
2023-06-21 13:00:14 -0700

    build: bump solidity version ^0.8.18
    Signed-off-by: Bryan Cole <bryan.cole@semiotic.ai>

--- src/Collateral.sol
+++ src/Collateral.sol
@@ -1,6 +1,6 @@
 // Copyright 2023-, Semiotic AI, Inc.
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
