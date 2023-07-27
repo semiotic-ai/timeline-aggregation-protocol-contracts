53374cd0998d6002b20c610216512ef72018cf05
Bryan Cole <bryan.cole@semiotic.ai>
2023-07-27 09:58:28 -0700

    docs(allocationid-tracker): update proof description to be more clear
    the previous wording could be mistaken to mean the proof is signed using receivers private key, this wording is intended to show that it is signed with the private key associated with the allocation ID
    
    Signed-off-by: Bryan Cole <bryan.cole@semiotic.ai>

--- src/AllocationIDTracker.sol
+++ src/AllocationIDTracker.sol
@@ -47,7 +47,7 @@ contract AllocationIDTracker {
      * @dev Marks an allocation ID as used.
      * @param sender The sender of the token to receiver.
      * @param allocationID The allocation ID to mark as used.
-     * @param proof ECDSA Proof signed by the receiver consisting of packed (sender address, allocationID, collateral contract address).
+     * @param proof ECDSA Proof signed by the receiver's allocationID consisting of packed (sender address, allocationID, collateral contract address).
      * @notice REVERT with error:
      *               - AllocationIDPreviouslyClaimed: If the allocation ID was previously claimed
      *               - InvalidProof: If the proof is not valid
@@ -68,7 +68,7 @@ contract AllocationIDTracker {
 
     /**
      * @dev Verifies a proof.
-     * @param proof ECDSA Proof signed by the receiver consisting of packed (sender address, allocationID, collateral contract address).
+     * @param proof ECDSA Proof signed by the receiver's allocationID consisting of packed (sender address, allocationID, collateral contract address).
      * @param sender The sender of the token to receiver.
      * @param allocationID The allocation ID to verify.
      * @notice REVERT with error:
