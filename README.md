b8cdec443ab0bc0345784ebe5d2f681f8c82cd80
moro <ricardoromero4444@gmail.com>
2023-12-11 16:16:33 -0300

    feat: add bindings.ts file with network checks
    Signed-off-by: moro <ricardoromero4444@gmail.com>
    Signed-off-by: Carlos V <carlos.vdr@semiotic.ai>
    
    Signed-off-by: Carlos V <carlos.vdr@semiotic.ai>

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Timeline Aggregation Protocol (TAP) Contract(s)
+
+## Create types and bindings
+Run the following commands
+```
+yarn build
+yarn bindings
+```
---
+++ src/bindings.ts
@@ -0,0 +1,72 @@
+// Copyright 2023-, Semiotic AI, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import { providers, Signer } from "ethers";
+
+// Contract addresses
+import * as DEPLOYED_CONTRACTS from "../addresses.json";
+
+// Contract ABIs
+import { TAPVerifier } from "../generated/ts-bindings/TAPVerifier";
+import { AllocationIDTracker } from "../generated/ts-bindings/AllocationIDTracker";
+import { Escrow } from "../generated/ts-bindings/Escrow";
+
+// Contract factories
+import { TAPVerifier__factory } from "../generated/ts-bindings/factories/TAPVerifier__factory";
+import { AllocationIDTracker__factory } from "../generated/ts-bindings/factories/AllocationIDTracker__factory";
+import { Escrow__factory } from "../generated/ts-bindings/factories/Escrow__factory";
+
+export { connectContracts };
+export type { NetworkContracts, DeployedContracts };
+
+type DeployedContracts = typeof DEPLOYED_CONTRACTS;
+
+interface NetworkContracts {
+  escrow: Escrow;
+  tapVerifier: TAPVerifier;
+  allocationIDTracker: AllocationIDTracker;
+}
+
+const connectContracts = async (
+  providerOrSigner: providers.Provider | Signer,
+  chainId: number
+): Promise<NetworkContracts> => {
+  const stringifiedChainId = `${chainId}`;
+  if (!(stringifiedChainId in DEPLOYED_CONTRACTS))
+    throw new Error(`chainId: '${chainId}' has no deployed contracts`);
+
+  const deployedContracts =
+    DEPLOYED_CONTRACTS[stringifiedChainId as keyof typeof DEPLOYED_CONTRACTS];
+
+  const getContractAddress = (contractName: keyof typeof deployedContracts) => {
+    if (!deployedContracts[contractName]) {
+      throw new Error(
+        `Deployed contract '${contractName}' is undefined for chainId: '${chainId}'`
+      );
+    }
+    const address = deployedContracts[contractName];
+
+    if (!address) {
+      throw new Error(
+        `Deployed contract '${contractName}' address is undefined for chainId: '${chainId}'`
+      );
+    }
+    return address;
+  };
+
+  const contracts: NetworkContracts = {
+    escrow: Escrow__factory.connect(
+      getContractAddress("Escrow"),
+      providerOrSigner
+    ),
+    tapVerifier: TAPVerifier__factory.connect(
+      getContractAddress("TAPVerifier"),
+      providerOrSigner
+    ),
+    allocationIDTracker: AllocationIDTracker__factory.connect(
+      getContractAddress("AllocationIDTracker"),
+      providerOrSigner
+    ),
+  };
+
+  return contracts;
+};
